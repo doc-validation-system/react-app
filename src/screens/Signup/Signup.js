5b65a04904e82741ce5f4b1e8d6785bd345c5541
HALDERKAUSTAV <kaustavh2016@gmail.com>
2022-12-14 22:11:09 +0530

    Signup screen update

--- src/screens/Signup/Signup.js
+++ src/screens/Signup/Signup.js
@@ -1,36 +1,75 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import styles from './Signup.module.css';
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import styles from "./Signup.module.css";
+import TextField from "@mui/material/TextField";
 
-
-const SignupCard = ()=>{
+const SignupCard = () => {
   return (
-    <Card sx={{ minWidth: 275 }}>
+    <Card sx={{ width: '40%' }}>
       <CardContent>
-        <Typography sx={{ fontSize: 20 }} color="text.secondary" gutterBottom>
+        <Typography
+          sx={{ fontSize: 20 }}
+          color="#286ADE"
+          gutterBottom
+          className={styles.head}
+        >
           Signup
         </Typography>
         <div className={styles.border} />
+        <div className={styles.subCardDiv}>
+        <CardActions>
+          <TextField
+            className={styles.box}
+            label="Username"
+            variant="outlined"
+          />
+        </CardActions>
+        <CardActions>
+          <TextField className={styles.box} label="Email" variant="outlined" />
+        </CardActions>
+        <CardActions>
+          <TextField
+            className={styles.box}
+            label="Organization Name"
+            variant="outlined"
+          />
+        </CardActions>
+        <CardActions>
+          <TextField
+            name="Password"
+            type="password"
+            placeholder="Password"
+            label="Password"
+          />
+        </CardActions>
+        <CardActions>
+          <TextField
+            name="Confirm Password"
+            type="password"
+            placeholder="Password"
+            label=" Confirm Password"
+          />
+        </CardActions>
+        </div>
+        <CardActions>
+      
+        </CardActions>
       </CardContent>
-      <CardActions>
-        <Button variant="outlined" className={styles.button}>Signup</Button>
-      </CardActions>
     </Card>
   );
-}
+};
 
 const SignupScreen = () => {
-  return(
+  return (
     <div className={styles.maindivStyle}>
       <header className={styles.divheader}>
         <SignupCard />
       </header>
     </div>
   );
-}
+};
 
-export default SignupScreen
+export default SignupScreen;
