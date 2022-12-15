36129dfa007aff37d041229bc67ff534680b7a37
HALDERKAUSTAV <kaustavh2016@gmail.com>
2022-12-15 17:44:03 +0530

    Signup update

---
+++ src/button/button.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+//import style from "./button.css";
+//import  Button  from "@material-ui/core";
+
+function signUpButton(){
+    return(
+        <div>
+            <button type="submit">Signup</button>
+        </div>
+    )
+}
+
+export default signUpButton;
--- src/screens/Signup/Signup.js
+++ src/screens/Signup/Signup.js
@@ -5,37 +5,66 @@ import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import styles from "./Signup.module.css";
 import TextField from "@mui/material/TextField";
-import Button from '@mui/material/Button'
-
+import signUpButton from "../../button/button";
 
 const SignupCard = () => {
   return (
-    <Card sx={{ width: '40%' }} className={styles.parentCard}>
+    <Card sx={{ width: "40%" }} className={styles.parentCard}>
       <CardContent>
-        <Typography sx={{ fontSize: 20 }} color="#286ADE" gutterBottom className={styles.head}>
+        <Typography
+          sx={{ fontSize: 20 }}
+          color="#286ADE"
+          gutterBottom
+          className={styles.head}
+        >
           Signup
         </Typography>
         <div className={styles.border} />
         <div className={styles.subCardDiv}>
           <CardActions>
-            <TextField className={styles.box} label="Username" variant="outlined" />
+            <TextField
+              className={styles.box}
+              label="Username"
+              variant="outlined"
+            />
           </CardActions>
           <CardActions>
-            <TextField className={styles.box} label="Email" variant="outlined" />
+            <TextField
+              className={styles.box}
+              label="Email"
+              variant="outlined"
+            />
           </CardActions>
           <CardActions>
-            <TextField className={styles.box} label="Organization Name" variant="outlined" />
+            <TextField
+              className={styles.box}
+              label="Organization Name"
+              variant="outlined"
+            />
           </CardActions>
           <CardActions>
-            <TextField className={styles.box} name="Password" type="password" placeholder="Password" label="Password" />
+            <TextField
+              className={styles.box}
+              name="Password"
+              type="password"
+              placeholder="Password"
+              label="Password"
+            />
           </CardActions>
           <CardActions>
-            <TextField className={styles.box} name="Confirm Password" type="password" placeholder="Password" label=" Confirm Password" />
+            <TextField
+              className={styles.box}
+              name="Confirm Password"
+              type="password"
+              placeholder="Password"
+              label=" Confirm Password"
+            />
           </CardActions>
         </div>
-        <CardActions style={{ justifyContent: 'center' }}>
+        {/* <CardActions style={{ justifyContent: 'center' }}>
           <Button className={styles.Buttons1} variant="contained">Signup</Button>
-        </CardActions>
+        </CardActions>  */}
+        <signUpButton />
       </CardContent>
     </Card>
   );
