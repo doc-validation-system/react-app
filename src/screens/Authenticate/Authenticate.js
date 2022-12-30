ef0988dffb75d5ba27401358aa25829e09c35d6b
Swapnodeep Biswas <bonny2610@gmail.com>
2022-12-30 17:18:01 +0530

    Modified some files

--- src/screens/Authenticate/Authenticate.js
+++ src/screens/Authenticate/Authenticate.js
@@ -1,33 +1,20 @@
 import React from "react";
 import styles from "./Authenticate.module.css";
-import AuthRouter from "./AuthRouter";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignupSection from "../Signup/Signup";
+import LoginSection from "../Login/Login";
+import HomeScreen from "../Home/Home";
 
 function Authentication() {
   return (
     <div className={styles.authPage}>
-       {/* Header with Logo */}
-       {/* <header className={`${styles.flex} ${styles.header}`}>
-         <img
-    //       src="./Images/DocValidateAPI-logo.png" */}
-           {/* alt="DocValidateLogo"
-           className={styles.logoImage}
-         />
-       </header> */}
-
-      {/* Signup section */}
-      {/* Routing will be done here */}
-      {/* <SignupSection /> */}
-      {/* <LoginSection/> */}
-      <AuthRouter/>
-
-       {/* Footer with Project team info
-       <footer className={`${styles.flex} ${styles.footer}`}>
-         <p className={styles.footer__text}>
-           © 2022 DocValidateAPI. All rights reserved. <br />A project by Aditi
-           Chatterjee, Charchika Biswas, Kaustav Halder, Sourashis Paul and
-           Swapnodeep Biswas
-         </p>
-       </footer> */}
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<HomeScreen />} />
+          <Route exact path="/signup" element={<SignupSection />} />
+          <Route exact path="/login" element={<LoginSection />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
