02c79a61569cd405483cc14d8771b8383001838c
Swapnodeep Biswas <bonny2610@gmail.com>
2023-02-17 18:42:36 +0530

    Working on TestAPI UI

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import SignupSection from "./screens/Signup/Signup";
 import LoginSection from "./screens/Login/Login";
 import HomeScreen from "./screens/Home/Home";
 import PrivateRoute from "./screens/PrivateRoute/PrivateRoute";
+import TestAPISection from "./screens/TestAPI/TestAPI";
 
 function App() {
   return (
@@ -11,8 +12,17 @@ function App() {
         <Route exact path="/" element={<HomeScreen />} />
         <Route exact path="/signup" element={<SignupSection />} />
         <Route exact path="/login" element={<LoginSection />} />
-        <Route exact path="/profile" element={<PrivateRoute routeName={{path: "/profile"}}/>} />
-        <Route exact path="/dashboard" element={<PrivateRoute routeName={{path: "/dashboard"}} />} />
+        <Route
+          exact
+          path="/profile"
+          element={<PrivateRoute routeName={{ path: "/profile" }} />}
+        />
+        <Route
+          exact
+          path="/dashboard"
+          element={<PrivateRoute routeName={{ path: "/dashboard" }} />}
+        />
+        <Route exact path="/testApi" element={<TestAPISection />} />
       </Routes>
     </BrowserRouter>
   );
