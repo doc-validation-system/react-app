f67dea3590ff9349b894486aea5836417fa210a1
HALDERKAUSTAV <kaustavh2016@gmail.com>
2023-02-19 23:47:58 +0530

    Fixed loader problem

--- src/screens/Dashboard/Dashboard.js
+++ src/screens/Dashboard/Dashboard.js
@@ -1,72 +1,104 @@
 import React from "react";
 import styles from "./Dashboard.module.css";
 import Card from "./Card";
+import Loader from "../../service/Loader/Loader";
 
 class DashboardSection extends React.Component {
-  handleProfile=async()=>{
-    const token=localStorage.getItem("token");
-    const email=localStorage.getItem("email");
-    var profileHeader=new Headers();
-    profileHeader.append("Authorization",'Bearer '+`${token}`);
+  constructor(props) {
+    super(props);
+    this.state = {
+      showSpinner: false
+    };
+  }
+  handleProfile = async () => {
+    this.setState({showSpinner:true});
+    const token = localStorage.getItem("token");
+    const email = localStorage.getItem("email");
+    var profileHeader = new Headers();
+    profileHeader.append("Authorization", 'Bearer ' + `${token}`);
     profileHeader.append("Content-Type", "application/x-www-form-urlencoded");
     var profileRequestOptions = {
       method: 'GET',
       headers: profileHeader,
       redirect: 'follow'
-      }
-    var profileResponse= await fetch(`https://api-docvalidation.onrender.com/user/profile/${email}`, profileRequestOptions);
+    }
+    var profileResponse = await fetch(`https://api-docvalidation.onrender.com/user/profile/${email}`, profileRequestOptions);
     var profileData = JSON.parse(await profileResponse.text());
-    if(profileResponse.status===200){
-      window.open("/profile","_self")
-    }     
+    if (profileResponse.status === 200) {
+      window.open("/profile", "_self");
+      this.setState({showSpinner:false});
+    }
+    else{
+      this.setState({showSpinner:false});
+    }
   }
   render() {
-    return (
-      <div className={styles.dashboardPage}>
-        {/* Header with Logo and Profile Icon */}
-        <header className={styles.header}>
-          <img
-            src="./Images/DocValidateAPI-logo.png"
-            alt="DocValidateLogo"
-            className={styles.logoImage}
-          />
-          <img
-            src="./Images/ProfileIcon.png"
-            alt="ProfileIcon"
-            className={styles.profileIcon}
-            onClick= { this.handleProfile}
-          />
-        </header>
+    return ( 
+    <>
+        {this.state.showSpinner ? (
+          <>
+            <div>
+              {/* <Backdrop
+              sx={{
+                color: "#fff",
+                zIndex: (theme) => theme.zIndex.drawer + 1,
+              }}
+              open={this.state.showSpinner}
+            >
+              <CircularProgress color="inherit" />
+            </Backdrop> */}
+              <Loader />
 
-        <section className={styles.dashboardCards}>
-          <Card
-            cardData={{
-              cardImage: "./Images/ApiKeyImg.png",
-              cardText: "Generate API Key",
-            }}
-          />
-          <Card
-            cardData={{
-              cardImage: "./Images/TestApiImg.png",
-              cardText: "Test API",
-            }}
-          />
-          <Card
-            cardData={{ cardImage: "./Images/DocsImg.png", cardText: "Docs" }}
-          />
-        </section>
+            </div>
+          </>
+        ) : (
+          <div className={styles.dashboardPage}>
+            {/* Header with Logo and Profile Icon */}
+            <header className={styles.header}>
+              <img
+                src="./Images/DocValidateAPI-logo.png"
+                alt="DocValidateLogo"
+                className={styles.logoImage}
+              />
+              <img
+                src="./Images/ProfileIcon.png"
+                alt="ProfileIcon"
+                className={styles.profileIcon}
+                onClick={this.handleProfile}
+              />
+            </header>
 
-        {/* Footer with Project team info*/}
-        <footer className={styles.footer}>
-          <p className={styles.footer__text}>
-            © 2022 DocValidateAPI. All rights reserved. <br />A project by Aditi
-            Chatterjee, Charchika Biswas, Kaustav Halder, Sourashis Paul and
-            Swapnodeep Biswas
-          </p>
-        </footer>
-      </div>
-    );
+            <section className={styles.dashboardCards}>
+              <Card
+                cardData={{
+                  cardImage: "./Images/ApiKeyImg.png",
+                  cardText: "Generate API Key",
+                }}
+              />
+              <Card
+                cardData={{
+                  cardImage: "./Images/TestApiImg.png",
+                  cardText: "Test API",
+                }}
+              />
+              <Card
+                cardData={{ cardImage: "./Images/DocsImg.png", cardText: "Docs" }}
+              />
+            </section>
+
+            {/* Footer with Project team info*/}
+            <footer className={styles.footer}>
+              <p className={styles.footer__text}>
+                © 2022 DocValidateAPI. All rights reserved. <br />A project by Aditi
+                Chatterjee, Charchika Biswas, Kaustav Halder, Sourashis Paul and
+                Swapnodeep Biswas
+              </p>
+            </footer>
+          </div>
+        )}
+        </>
+        );
   }
 }
 
-export default DashboardSection;
+        export default DashboardSection;
--- src/service/Loader/Loader.js
+++ src/service/Loader/Loader.js
@@ -1,15 +1,22 @@
 import React from "react";
 import styles from "./Loader.module.css";
 
-class Loader extends React.Component{
+class Loader extends React.Component {
     render() {
         return (
-            <div className={styles.spinner}>
-                <div className={styles.rect1}></div>
-                <div className={styles.rect2}></div>
-                <div className={styles.rect3}></div>
-                <div className={styles.rect4}></div>
-                <div className={styles.rect5}></div>
+            <div className={styles.loaderSection}>
+                <div className={styles.spinner}>
+                    <div className={styles.rect1}></div>
+                    <div className={styles.rect2}></div>
+                    <div className={styles.rect3}></div>
+                    <div className={styles.rect4}></div>
+                    <div className={styles.rect5}></div>
+                </div>
+                <img
+                    src="./Images/DocValidateAPI-logo.png"
+                    alt="DocValidateLogo"
+                    className={styles.logoImage}
+                />
             </div>
         )
     }
