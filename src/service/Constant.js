58e14855fbc5e3e20e8b0a8345440a8fe6f9f30f
Swapnodeep Biswas <bonny2610@gmail.com>
2023-05-06 22:10:48 +0530

    Updated TestAPI functionalities

--- src/service/Constant.js
+++ src/service/Constant.js
@@ -1,3 +1,5 @@
-const url = "https://e212-2405-201-800e-f8b9-d969-e07d-b97e-4fbd.in.ngrok.io";
+const url = "https://da9c-2405-201-800e-f8f7-98a6-78a4-51d3-aa84.in.ngrok.io";
+
+// const url = "http://localhost:6001";
 
 export default url;
